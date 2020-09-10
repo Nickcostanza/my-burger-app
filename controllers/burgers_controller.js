9ae66b4e898bb73fa6d5237bc044ef6e6239cd55
Nick Costanza <nicholas.g.costanza@gmail.com>
2020-09-10 12:54:09 -0700

    added post route

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -11,3 +11,12 @@ router.get('/', function(req, res) {
         res.render('index', hbsObject);
     });
 });
+
+router.post("/burger", function(req, res) {
+    burger.insertOne(['burger_name'],
+    [req.body.burger_name], 
+    function(result) {
+        res.status(200).end();
+    })
+});
+     
