c7db17c5599c8dcdbd535bc77d6b92b81a4924b6
Nick Costanza <nicholas.g.costanza@gmail.com>
2020-12-10 08:49:07 -0800

    connected to db and showing on page, but devour button and put method not working

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -3,11 +3,9 @@ const router = express.Router();
 
 const burger = require('../models/burger.js');
 
-
-
-//select all
-router.get('/', function(req, res) {
-    burger.selectAll(function(data) {
+//Select all
+router.get('/', (req, res) => {
+    burger.selectAll(function (data) {
         let hbsObject = {
             burgers: data
         };
@@ -15,25 +13,29 @@ router.get('/', function(req, res) {
     });
 });
 
-//insert one
+
+//Insert One
 router.post('/burger', (req, res) => {
-    burger.insertOne(['burger_name'],
-    [req.body.burger_name], 
-    function(result) {
-        res.status(200).end();
+    burger.insertOne(
+        ['burger_name',],
+        [req.body.burger_name,],
+        function(result){
+            res.status(200).end();
+        });
+    });
+
+//Update 
+router.put('/burgers/:id', (req, res) => {
+    let condition = req.params.id
+    console.log('condition', condition)
+    burger.updateOne(
+        //changes the devoured val to true 
+        {devoured: true},
+        condition,
+        function (result) {
+            res.status(200).end();
     })
 });
-  
 
-//update
-router.put('/burger/:id'), function(req, res) {
-    var condition = req.params.id;
-    burger.updateOne(
-        {devoured: true},
-        condition, 
-        function(result) {
-            res.status(200).end();
-        })
-};
 
 module.exports = router;
